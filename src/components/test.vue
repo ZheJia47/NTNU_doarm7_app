1c731ecdbf5d5ffb19fb93d211398fc81d5ef57d
ZheJia47 <53133006+ZheJia47@users.noreply.github.com>
2021-01-07 17:17:01 +0800

    test.vue add search bar

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,6 +1,13 @@
 <template>
   <div>     
-
+    
+    <input      
+      v-model="input1" 
+      placeholder="請輸入日期 (yyyy-mm-dd)"
+    >
+    <button @click="getData(input1)">確定</button>
+        
+    <!-- table -->
     <table>
       <thead>
         <tr class='thColor'>
@@ -26,19 +33,25 @@ export default {
   data() {
     return {      
       dataJson: [],
-      
+      input1: '',
+            
     };
   },  
   
   mounted() {    
-    fetch('../../static/師大學七用水紀錄/2020/11月/daily_data_for_each_room/2020-11-01的用水記錄(calibrated).json')
-     .then(res => res.json())
-     .then(contents => this.dataJson = contents)
+    // fetch('../../static/師大學七用水紀錄/2020/11月/daily_data_for_each_room/2020-11-01的用水記錄(calibrated).json')
+    //  .then(res => res.json())
+    //  .then(contents => this.dataJson = contents)
   },
 
   methods: {
-    getData(){
-      return 0
+    getData(date){
+      var date1 = '../../static/師大學七用水紀錄/2020/11月/daily_data_for_each_room/' + 
+        String(date)+
+        '的用水記錄(calibrated).json'
+      fetch(date1)
+      .then(res => res.json())
+      .then(contents => this.dataJson = contents)      
     }
 
   }
@@ -50,7 +63,7 @@ export default {
 <style scoped>
   .thColor {color: rgb(24, 141, 219)}
   table {
-    width: 30%;  
+    width: 30%;      
   }
 
   th {
@@ -68,5 +81,19 @@ export default {
     text-align: center;
     padding: 10px 0;
   }
-    
+
+  input {    
+    width: 10%;
+    margin: 10px 0px 10px 3px; /* top right bottom left */
+    padding: 2px;
+    border: 2px solid rgb(45, 121, 235);
+  
+  }
+
+  button {    
+    background-color: rgb(78, 77, 77);
+    margin: 0px 0px 10px; /* top left-right bottom */
+  }
+
+  
 </style>
